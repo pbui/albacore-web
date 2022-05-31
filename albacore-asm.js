1ee0c444a39dd4784ca2c76e74217112c15e1a36
Peter Bui <pbui@bx612.space>
2022-05-31 16:40:08 -0400

    albacore-asm: refactor assembler into multiple phases
    Now emits memory

--- albacore-asm.js
+++ albacore-asm.js
@@ -2,7 +2,7 @@
 
 /* Constants */
 
-const MEMORY_SEGMENT = Object.freeze({
+const MEMORY_SEGMENTS = Object.freeze({
     text: 0,
     data: 1
 });
@@ -10,94 +10,152 @@ const MEMORY_SEGMENT = Object.freeze({
 /* Classes */
 
 class TextInstruction {
-    constructor(binary, source) {
-    	this.binary = binary;
-    	this.source = source;
+    constructor(number, binary, source, label) {
+        this.number = number;
+        this.binary = binary;
+        this.source = source;
+        this.label  = label;
+    }
+
+    toString() {
+        let number = this.number.toString(16).padStart(4, "0");
+        let label  = this.label ? `\ttarget label: ${this.label}` : "";
+        return `@${number} ${this.binary} // ${this.source}${label}`;
     }
 }
 
 class Assembler {
     constructor() {
-    	this.initialize();
-    	
-    	this.text_instructions = [
-	    {regex: /add\s+(?<rw>r[0-9]+)\s*,\s*(?<ra>r[0-9]+),\s*(?<rb>r[0-9]+)/, assembler: this.assemble_add.bind(this)},
-	    {regex: /ldi\s+(?<rw>r[0-9]+)\s*,\s*(?<imm8>[0-9a-zA-ZxX]+)/         , assembler: this.assemble_ldi.bind(this)}
-    	];
+        this.initialize();
+
+        this.text_instructions = [
+            {regex: /add\s+(?<rw>r[0-9]+)\s*,\s*(?<ra>r[0-9]+),\s*(?<rb>r[0-9]+)/, assembler: this.assemble_add.bind(this)},
+            {regex: /ldi\s+(?<rw>r[0-9]+)\s*,\s*(?<imm8>[0-9a-zA-ZxX]+)/         , assembler: this.assemble_ldi.bind(this)},
+            {regex: /quit/                                                       , assembler: this.assemble_quit.bind(this)}
+        ];
     }
 
     initialize() {
-	// Initialize memory segments and default to parsing text segment
-	this.memory_text    = [];
-	this.memory_data    = [];
-	this.memory_segment = MEMORY_SEGMENT.text;
+        // Initialize memory segments and default to parsing text segment
+        this.data_memory = [];
+        this.text_memory = [];
     }
 
+    // Assembly methods
+
     assemble_lop(op, rw, ra, rb) {
-	rw = parseInt(rw.slice(1)).toString(16);
-	ra = parseInt(ra.slice(1)).toString(16);
-	rb = parseInt(rb.slice(1)).toString(16);
-	return op + rw + ra + rb;
+        rw = parseInt(rw.slice(1)).toString(16);
+        ra = parseInt(ra.slice(1)).toString(16);
+        rb = parseInt(rb.slice(1)).toString(16);
+        return op + rw + ra + rb;
     }
 
     assemble_add(rw, ra, rb) {
-    	return this.assemble_lop("0", rw, ra, rb);
+        return this.assemble_lop("0", rw, ra, rb);
     }
 
     assemble_ldi(rw, imm8) {
-	rw = parseInt(rw.slice(1)).toString(16);
-	if (imm8.startsWith("0x")) {
-	    imm8 = imm8.slice(2).padStart(2, "0");
-	} else {
-	    imm8 = parseInt(imm8).toString(16).padStart(2, "0");
-	}
-	return "7" + rw + imm8;
+        rw = parseInt(rw.slice(1)).toString(16);
+        if (imm8.startsWith("0x")) {
+            imm8 = imm8.slice(2).padStart(2, "0");
+        } else {
+            imm8 = parseInt(imm8).toString(16).padStart(2, "0");
+        }
+        return "7" + rw + imm8;
     }
-    
+
+    assemble_quit() {
+        return "f000";
+    }
+
+    // Parsing methods
+
+    parse_segment_lines(source_lines) {
+        let data_lines = [];
+        let text_lines = [];
+        let memory_segment = MEMORY_SEGMENTS.text;
+
+        for (let source_line of source_lines) {
+            // Trim whitespace and C++ comments
+            // TODO: Skip C style comments
+            source_line = source_line.trim().replace(/\/\/.*$/, '');
+
+            // Skip empty lines
+            if (!source_line) {
+                continue;
+            }
+
+            if (source_line == '.data') {
+                memory_segment = MEMORY_SEGMENTS.data;
+            } else if (source_line == '.text') {
+                memory_segment = MEMORY_SEGMENTS.text;
+            } else {
+                if (memory_segment == MEMORY_SEGMENTS.data) {
+                    data_lines.push(source_line);
+                } else {
+                    text_lines.push(source_line);
+                }
+            }
+        }
+
+        return [data_lines, text_lines];
+    }
+
+    // Memory segment assembly methods
+
+    assemble_data_segment(data_lines) {
+        // TODO: Parse data literals and arrays
+        return true;
+    }
+
+    assemble_text_segment(text_lines) {
+        for (let number = 0; number < text_lines.length; number++) {
+            let source = text_lines[number];
+            let foundInstruction = false;
+
+            for (const text_instruction of this.text_instructions) {
+                let match = source.match(text_instruction.regex);
+                if (match) {
+                    const binary = text_instruction.assembler(...match.slice(1));
+                    const label  = "";      // TODO: figure out label
+
+                    if (source == "quit") { // XXX: Is this a hack?
+                        source = "sys 0";
+                    }
+
+                    this.text_memory.push(new TextInstruction(
+                        number, binary, source, label
+                    ));
+                    foundInstruction = true;
+                }
+            }
+
+            if (!foundInstruction) {
+                console.log(`Invalid text instruction: ${text_line}`);
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // Static Assembly method
+
     static assemble_source() {
-    	let assembler = new Assembler();
+        let assembler = new Assembler();
 
-	// Extract assembly text from text area and split into lines
-	let source_text  = document.getElementById("albacore_source").value;
-	let source_lines = source_text.split('\n');
+        // Extract assembly text from text area and split into lines
+        const source_textarea = document.getElementById("albacore_source");
+        const source_lines    = source_textarea.value.split('\n');
 
-	for (let line of source_lines) {
-	    // Remove whitespace
-	    // TODO: Trim comments
-	    line = line.trim();
+        const [data_lines, text_lines] = assembler.parse_segment_lines(source_lines);
 
-	    // Skip empty lines and C++ style comments
-	    // TODO: Skip C style comments
-	    if (!line || line.startsWith("//")) {
-		continue;
-	    }
+        assembler.assemble_data_segment(data_lines);
+        assembler.assemble_text_segment(text_lines);
 
-	    // Switch memory segments
-	    if (line == '.text') {	
-		assembler.memory_segment = MEMORY_SEGMENT.text;
-		continue;
-	    } else if (line == '.data') {	
-		assembler.memory_segment = MEMORY_SEGMENT.data;
-		continue;
-	    }
-
-	    let foundMatch = false;
-
-	    if (assembler.memory_segment == MEMORY_SEGMENT.text) {
-		// TODO: Handle errors (ie. not enough arguments)
-		for (let text_instruction of assembler.text_instructions) {
-		    const match = line.match(text_instruction.regex);
-		    if (match) {
-			let binary = text_instruction.assembler(...match.slice(1));
-			console.log(`${binary} // ${line}`);
-			foundMatch = true;
-		    }
-		}
-	    }
-
-	    if (!foundMatch) {
-	    	console.log(`Invalid instruction: ${line}`);
-	    }
-	}
+        var memory_textarea   = document.getElementById("albacore_memory");
+        memory_textarea.value = "// .text\n" + assembler.text_memory.join('\n');
     }
 }
+
+// vim: sts=4 sw=4 ts=8 expandtab ft=javascript
